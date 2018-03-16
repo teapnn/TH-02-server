7ddea8907d42ea23d62eafc96337541953d59b24
teapnn <teapnn@gmail.com>
2018-03-16 17:56:08 +0700

    first commit

---
+++ LTM-02-Server/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace LTM_02_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtMS = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(339, 188);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(61, 34);
+            this.button5.TabIndex = 37;
+            this.button5.Text = "Images";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(339, 115);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 34);
+            this.button4.TabIndex = 36;
+            this.button4.Text = "Musics";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(339, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 34);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "File";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // txtMS
+            // 
+            this.txtMS.Location = new System.Drawing.Point(-1, 259);
+            this.txtMS.Multiline = true;
+            this.txtMS.Name = "txtMS";
+            this.txtMS.Size = new System.Drawing.Size(319, 65);
+            this.txtMS.TabIndex = 34;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 33;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-4, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Message";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(-1, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(319, 147);
+            this.listBox1.TabIndex = 31;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "IP Client";
+            // 
+            // ButSend
+            // 
+            this.ButSend.Location = new System.Drawing.Point(339, 257);
+            this.ButSend.Name = "ButSend";
+            this.ButSend.Size = new System.Drawing.Size(75, 67);
+            this.ButSend.TabIndex = 29;
+            this.ButSend.Text = "Send";
+            this.ButSend.UseVisualStyleBackColor = true;
+            this.ButSend.Click += new System.EventHandler(this.ButSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 349);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtMS);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtMS;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButSend;
+    }
+}
+
